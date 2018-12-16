732c8183db2524f2d1753239932090d590954e00
ZakharovVitaly <egonthrope@gmail.com>
2018-12-16 11:57:08 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+Script retrieves weather data from https://openweathermap.org/api
+It uses the current weather data API: https://openweathermap.org/current and uses pyown: https://pypi.python.org/pypi/pyowm
+The python script uses no arguments, using only environment variables:
+OPENWEATHER_API_KEY
+CITY_NAME
+
+Example:
+export OPENWEATHER_API_KEY="xxxxxxxxxxxx"
+export CITY_NAME="Moscow"
+
+./getweather.py
+
+source=openweathermap, city="Moscow", description="few clouds", temp=10.2, humidity=75
---
+++ currentweather.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import pyowm
+import os
+import sys
+
+def celsius(kelvin):
+    return kelvin-273.15
+
+def fahrenheit(kelvin):
+    return (9/5)*(kelvin-273.15) + 32
+
+if __name__ == "__main__":
+
+    city_var = 'CITY_NAME'
+    try:
+        city = os.environ[city_var]
+    except KeyError:
+        print city_var + ': No city pointed'
+        sys.exit(1)
+
+    api_key = 'OPENWEATHER_API_KEY'
+    try:
+        api_key = os.environ[api_key]
+    except KeyError:
+        print api_key + ': No API key added'
+        sys.exit(1)
+
+    owm = pyowm.OWM(API_key=api_key, version='2.5')
+    weather = owm.weather_at_place(city)
+    w = weather.get_weather()
+    print 'source=openweathermap,',\
+          'city=' + '"' + city + '",',\
+          'description=' + '"' +  str(w.get_detailed_status()) + '",',\
+          'temp=' + str(celsius(w.get_temperature()['temp'])) + ",",\
+          'humidity=' + str(w.get_humidity())
